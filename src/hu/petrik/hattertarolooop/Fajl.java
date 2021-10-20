b17523643158c001d50baf90c5f8a5c5d334d992
Budaházi Bence Nándor <bence0630@gmail.com>
2021-10-20 20:13:49 +0200

    Első feladat befejezve

--- src/hu/petrik/hattertarolooop/Fajl.java
+++ src/hu/petrik/hattertarolooop/Fajl.java
@@ -3,8 +3,96 @@ package hu.petrik.hattertarolooop;
 import java.util.List;
 
 public class Fajl {
-    String nev;
-    String kiterjesztes;
-    String tartalom;
-    List<Attributum> attributumok;
+    private String nev;
+    private String kiterjesztes;
+    private String tartalom;
+    private List<Attributum> attributumok;
+    private Meret meret;
+
+    public Fajl(String nev, String kiterjesztes, String tartalom, List<Attributum> attributumok) {
+        this.nev = nev;
+        this.kiterjesztes = kiterjesztes;
+        this.tartalom = tartalom;
+        this.attributumok = attributumok;
+        int mertEgysegIndex = 0;
+        long meret = tartalom.length();
+        while (meret > 1024 && mertEgysegIndex < 4) {
+            meret /= 1024;
+            mertEgysegIndex++;
+        }
+        MertekEgyseg mertekEgyseg;
+        switch (mertEgysegIndex) {
+            case 1:
+                mertekEgyseg = MertekEgyseg.KILOBYTE;
+                break;
+            case 2:
+                mertekEgyseg = MertekEgyseg.MEGABYTE;
+                break;
+            case 3:
+                mertekEgyseg = MertekEgyseg.GIGABYTE;
+                break;
+            case 4:
+                mertekEgyseg = MertekEgyseg.TERABYTE;
+                break;
+            default:
+                mertekEgyseg = MertekEgyseg.BYTE;
+                break;
+        }
+        this.meret = new Meret(meret, mertekEgyseg);
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public List<Attributum> getAttributumok() {
+        return attributumok;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public void setKiterjesztes(String kiterjesztes) {
+        this.kiterjesztes = kiterjesztes;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+        int mertEgysegIndex = 0;
+        long meret = tartalom.length();
+        while (meret > 1024 && mertEgysegIndex < 4) {
+            meret /= 1024;
+            mertEgysegIndex++;
+        }
+        MertekEgyseg mertekEgyseg;
+        switch (mertEgysegIndex) {
+            case 1:
+                mertekEgyseg = MertekEgyseg.KILOBYTE;
+                break;
+            case 2:
+                mertekEgyseg = MertekEgyseg.MEGABYTE;
+                break;
+            case 3:
+                mertekEgyseg = MertekEgyseg.GIGABYTE;
+                break;
+            case 4:
+                mertekEgyseg = MertekEgyseg.TERABYTE;
+                break;
+            default:
+                mertekEgyseg = MertekEgyseg.BYTE;
+                break;
+        }
+        this.meret.setMeret(meret);
+        this.meret.setMertekEgyseg(mertekEgyseg);
+    }
+
+    public void setAttributumok(List<Attributum> attributumok) {
+        this.attributumok = attributumok;
+    }
+
+    @Override
+    public String toString() {
+        return nev + "." + kiterjesztes + " " + meret;
+    }
 }
--- src/hu/petrik/hattertarolooop/Meret.java
+++ src/hu/petrik/hattertarolooop/Meret.java
@@ -1,13 +1,53 @@
 package hu.petrik.hattertarolooop;
 
 public class Meret {
-    long meret; //Méret változó bejelentése - deklaráció
-    MertekEgyseg mertekEgyseg;
+    private long meret; //Méret változó bejelentése - deklaráció
+    private MertekEgyseg mertekEgyseg;
 
     public Meret(long meret, MertekEgyseg mertekEgyseg) {
         this.meret = meret; //Méret változónak érték adás - inicializáció
         this.mertekEgyseg = mertekEgyseg;
     }
 
-    
+    public long getMeret() {
+        return meret;
+    }
+
+    public void setMeret(long meret) {
+        this.meret = meret;
+    }
+
+    public MertekEgyseg getMertekEgyseg() {
+        return mertekEgyseg;
+    }
+
+    public void setMertekEgyseg(MertekEgyseg mertekEgyseg) {
+        this.mertekEgyseg = mertekEgyseg;
+    }
+
+    public long getEredetiMeret() {
+        long meret = this.meret;
+        switch (this.mertekEgyseg) {
+            case BYTE:
+                break;
+            case KILOBYTE:
+                meret *= 1024;
+                break;
+            case MEGABYTE:
+                meret *= Math.pow(1024, 2);
+                break;
+            case GIGABYTE:
+                meret *= Math.pow(1024, 3);
+                break;
+            case TERABYTE:
+                meret *= Math.pow(1024, 4);
+                break;
+        }
+        return meret;
+    }
+
+    @Override
+    public String toString() {
+        return "Méret: " + this.meret + " " + this.mertekEgyseg;
+    }
 }
